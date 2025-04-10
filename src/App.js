86328f5c1be9ad9363a8029d1ba621bfc99aac36
vinodhsukumarvictor <gpgvictor@gmail.com>
2025-04-10 10:58:08 +0000

    changed colour in css

--- src/App.js
+++ src/App.js
@@ -35,7 +35,11 @@ function App() {
 
   return (
     <div className='header field'>
+<<<<<<< HEAD
       VAT CALCULATOR FOR VICTOR
+=======
+      VAT CALCULATOR FROM VICTOR
+>>>>>>> 8939ec6 (changed colour in css)
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
